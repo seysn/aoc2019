3961a7c29fff6ea177bcd93d9dc4dc6d54c896ae
Nicolas Seys <nicolas.seys@isabo.ai>
2019-12-09 10:23:58 +0100

    reworked IntCode

--- 05/main.py
+++ 05/main.py
@@ -1,68 +1,89 @@
-def instruction(opcode: str):
-    opcode = opcode.rjust(5, "0")
+class IntCode(object):
+    def __init__(self, ops):
+        self.ops = ops
+        self.idx = 0
+        self.opcodes = {
+            1: (3, self._add),
+            2: (3, self._mul),
+            3: (1, self._input),
+            4: (1, self._output),
+            5: (2, self._jump_if_true),
+            6: (2, self._jump_if_false),
+            7: (3, self._less_than),
+            8: (3, self._equals)
+        }
 
-    # opcode, mode 1st, mode 2nd, mode 3rd
-    return int(opcode[3:]), int(opcode[2]), int(opcode[1]), int(opcode[0])
+    def _add(self, modes, a, b, c):
+        self.ops[c] = int(self.ops[a] if modes[0] == 0 else a) + int(self.ops[b] if modes[1] == 0 else b)
 
-assert instruction("1002") == (2, 0, 1, 0)
+    def _mul(self, modes, a, b, c):
+        self.ops[c] = int(self.ops[a] if modes[0] == 0 else a) * int(self.ops[b] if modes[1] == 0 else b)
 
-def opcode(ops, pos=0):
-    op, fst, snd, thd = instruction(str(ops[pos]))
-    if op == 99:
-        return ops
+    def _input(self, modes, a):
+        self.ops[a] = int(input("Input Value: "))
 
-    if op in [1, 2, 7, 8]:
-        a, b, c = ops[pos+1:pos+4]
-        if op == 1:
-            ops[c] = int(ops[a] if fst == 0 else a) + int(ops[b] if snd == 0 else b)
-        elif op == 2:
-            ops[c] = int(ops[a] if fst == 0 else a) * int(ops[b] if snd == 0 else b)
-        elif op == 7:
-            if int(ops[a] if fst == 0 else a) < int(ops[b] if snd == 0 else b):
-                ops[c] = 1
+    def _output(self, modes, a):
+        print(f"Output Value: {self.ops[a]}")
+
+    def _jump_if_true(self, modes, a, b):
+        if int(self.ops[a] if modes[0] == 0 else a) != 0:
+            self.idx = int(self.ops[b] if modes[1] == 0 else b)
+
+    def _jump_if_false(self, modes, a, b):
+        if int(self.ops[a] if modes[0] == 0 else a) == 0:
+            self.idx = int(self.ops[b] if modes[1] == 0 else b)
+
+    def _less_than(self, modes, a, b, c):
+        if int(self.ops[a] if modes[0] == 0 else a) < int(self.ops[b] if modes[1] == 0 else b):
+            self.ops[c] = 1
+        else:
+            self.ops[c] = 0
+
+    def _equals(self, modes, a, b, c):
+        if int(self.ops[a] if modes[0] == 0 else a) == int(self.ops[b] if modes[1] == 0 else b):
+            self.ops[c] = 1
+        else:
+            self.ops[c] = 0
+
+    def instruction(self, opcode: int):
+        opcode = str(opcode).rjust(5, "0")
+
+        # opcode, mode 1st, mode 2nd, mode 3rd
+        return int(opcode[3:]), int(opcode[2]), int(opcode[1]), int(opcode[0])
+
+    def run(self):
+        while True:
+            op, fst, snd, thd = self.instruction(self.ops[self.idx])
+
+            if op == 99:
+                return
+
+            if op not in self.opcodes:
+                raise Exception(f"Invalid opcode {op} in position {self.idx}")
+
+            nparams, func = self.opcodes[op]
+            if nparams == 1:
+                a = self.ops[self.idx+1]
+                self.idx += 2
+                func((fst, snd, thd), a)
+            elif nparams == 2:
+                a, b = self.ops[self.idx+1:self.idx+3]
+                self.idx += 3
+                func((fst, snd, thd), a, b)
             else:
-                ops[c] = 0
-        elif op == 8:
-            if int(ops[a] if fst == 0 else a) == int(ops[b] if snd == 0 else b):
-                ops[c] = 1
-            else:
-                ops[c] = 0
-        return opcode(ops, pos + 4)
-    elif op in [3, 4]:
-        a = ops[pos+1]
-        if op == 3:
-            inp = input("Input Value: ")
-            ops[a] = int(inp)
-        elif op == 4:
-            print(f"Output Value: {ops[a]}")
+                a, b, c = self.ops[self.idx+1:self.idx+4]
+                self.idx += 4
+                func((fst, snd, thd), a, b, c)
 
-        return opcode(ops, pos + 2)
-    elif op in [5, 6]:
-        a, b = ops[pos+1:pos+3]
-        if op == 5:
-            # print("jump-if-true", a, b)
-            if int(ops[a] if fst == 0 else a) != 0:
-                return opcode(ops, int(ops[b] if snd == 0 else b))
-        elif op == 6:
-            # print("jump-if-false", a, b)
-            if int(ops[a] if fst == 0 else a) == 0:
-                return opcode(ops, int(ops[b] if snd == 0 else b))
-        return opcode(ops, pos + 3)
-    else:
-        raise Exception(f"Invalid opcode {op} in position {pos}")
-
-def part1(ops):
-    opcode(ops)
-
-def part2(ops):
-    return opcode(ops)
-
-def run(inp):
-    return opcode(list(map(int, inp.split(","))))
+def run(ops):
+    intcode = IntCode(ops)
+    intcode.run()
 
 if __name__ == "__main__":
     ops = []
     with open("05/input.txt") as f:
         ops = list(map(int, f.readline().split(",")))
-    #part1(ops) # Input Value: 1 | Output Value: 15508323
-    print(part2(ops))
+
+    # Part 1 : Input Value: 1 | Output Value: 15508323
+    # Part 2 : Input Value: 5 | Output Value: 9006327
+    run(ops)
